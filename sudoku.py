22efb495f524f16176be11324437c7bc178d4478
Bhagyashree <bbhattacharyya135@gmail.com>
2023-11-17 21:57:23 +0530

    modified

--- sudoku.py
+++ sudoku.py
@@ -8,7 +8,7 @@ pygame.font.init()
 screen = pygame.display.set_mode((500, 600))
  
 # Title and Icon 
-pygame.display.set_caption("SUDOKU SOLVER USING BACKTRACKING")
+pygame.display.set_caption("SUDOKU")
 img = pygame.image.load('icon.png')
 pygame.display.set_icon(img)
  
